b5c1214007edcc630fb055ad83a5ddad3a718ad7
Meriem1551 <meriem200409@gmail.com>
2023-10-05 06:54:37 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+int main(int argc, char *argv[])
+{
+    // the first arg must be the size
+    int size, *arr, i, j, *t;
+    size = atoi(argv[1]);
+    arr = (int *)malloc(size * sizeof(int));
+    t = (int *)malloc(size * sizeof(int));
+    if (arr == NULL)
+    {
+        return -1;
+    }
+    for (i = 2, j = 0; i < argc, j < size; i++, j++)
+    {
+        arr[j] = atoi(argv[i]);
+    }
+    t = Selection_sort(size, arr);
+    display_array(t, size);
+    free(arr);
+    free(t);
+    return 0;
+}
